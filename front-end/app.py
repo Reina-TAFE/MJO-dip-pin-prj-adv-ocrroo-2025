df4b40ffc63ad7f5ae9418acfc63d04f6ffaabc1
ST4T1K <mjobrien06@hotmail.com>
2025-11-13 21:06:58 +0800

    feat: update front-end GUI

--- front-end/app.py
+++ front-end/app.py
@@ -5,26 +5,28 @@ from flask import render_template, redirect
 from flask import request, url_for
 
 app = Flask(__name__)
-#NOTE: flask run --port 8001 --debug
+
+# NOTE: flask run --port 8001 --debug
 
 @app.route("/")
-def index(transcript = None):
-    print('we are here, sire')
-    video = "./resources/oop.mp4"
-    return render_template("index.html", transcript=transcript)
+def index():
+    transcript = request.args.get("transcript")
+
+    print(transcript)
+
+    if transcript:
+        return render_template("index.html", transcript=transcript)
+    else:
+        return render_template("index.html")
 
 
-@app.route("/generate-transcript/")
+@app.route("/generate-transcript", methods=["POST"])
 def get_transcript():
+    video_position = request.form['video_pos']
 
-    frame_number = request.args.get("video_pos")
-
-    url = f"http://127.0.0.1:8000/video/demo/frame/{frame_number}/ocr"
+    url = f"http://127.0.0.1:8000/video/demo/frame/{video_position}/ocr"
     response = requests.get(url).content
 
     response = response.decode(encoding='utf-8')
 
-    print(response)
-
-    #TODO replace with redirect
-    return  redirect(url_for('index', transcript=response))
+    return redirect(url_for('index', transcript=response))
